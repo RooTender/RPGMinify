103e27d591c38217d27144228de2810bcc7fa744
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-04-21 00:26:26 +0200

    Change to latin-1

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ audio_obsoletes = utils.get_files(audio_files_path)
 image_obsoletes = utils.get_files(image_files_path)
 
 for map in tqdm(os.listdir(maps_path), desc="Game assets dependencies"):
-    with open(os.path.join(maps_path, map), 'r', encoding="utf8") as file:
+    with open(os.path.join(maps_path, map), 'r', encoding='latin-1') as file:
         map_data = file.read()
 
     files = copy.copy(audio_obsoletes)
@@ -35,7 +35,7 @@ scripts_path = os.path.join('www', 'js')
 scripts = utils.get_files(scripts_path)
 
 for script in tqdm(scripts, desc="Engine & plugin dependencies"):
-    with open(script, 'r', encoding="utf8") as file:
+    with open(script, 'r', encoding='latin-1') as file:
         script_data = file.read();
 
     files = copy.copy(audio_obsoletes)
