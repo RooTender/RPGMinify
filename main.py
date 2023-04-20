1423cbe160a6b45c2c117c9a2745be94d53eb915
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2023-04-20 18:52:00 +0200

    Extract bgm and bgs filenames

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ if not os.path.exists(dependencies_maps):
 audio_files_path = os.path.join('www', 'audio')
 image_files_path = os.path.join('www', 'img')
 
-audio_files = utils.get_directories(audio_files_path)
-image_files = utils.get_directories(image_files_path)
+audio_dirs = utils.get_directories(audio_files_path)
+image_dirs = utils.get_directories(image_files_path)
 
+dependencies = []
 for dependency_map in os.listdir(dependencies_maps):
     with open(os.path.join(dependencies_maps, dependency_map), 'r') as file:
-        data = file.read()
+        json_file = file.read()
     
-    print(utils.find_values(data, 'bgm'))
-    utils.find_values(data, 'bgm')
-    break
+    for directory in audio_dirs:
+        for value in utils.find_values(json_file, directory):
+            data = utils.jsonify(str(value))
+            data = json.loads(data.encode('utf-8'))['name'] 
+            
+            if data not in dependencies and data is not '':
+                dependencies.append(data)
+
+    
+
+    
